a63532ed10b10f8548f83febea9050cc9fc86f22
arnout.engelen <arnout.engelen@c732d7ce-d5e6-11dd-918e-25ffe7a15323>
2008-12-29 22:18:59 +0000

    templated svggraphics2d class

---
+++ ical2svg/src/main/java/net/bzzt/ical2svg/TemplatedSVGGraphics2D.java
@@ -0,0 +1,46 @@
+package net.bzzt.ical2svg;
+
+import java.io.InputStream;
+import java.io.Writer;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.batik.svggen.SVGGraphics2D;
+import org.apache.batik.svggen.SVGGraphics2DIOException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ * An SVGGraphics2D that keeps the original template intact.
+ * 
+ * For some reason, we need to re-add the template's nodes
+ * when streaming out the document
+ * 
+ * @author arnouten
+ */
+public class TemplatedSVGGraphics2D extends SVGGraphics2D {
+
+	private final Document template;
+	
+	public TemplatedSVGGraphics2D(Document template) {
+		super(template);
+		this.template = template;
+	}
+
+	@Override
+	public void stream(Element svgRoot, Writer writer, boolean useCss,
+			boolean escaped) throws SVGGraphics2DIOException {
+		NodeList templateNodes = template.getChildNodes();
+		for (int i = 0; i < templateNodes.getLength(); i++)
+		{
+			Node templateNode = templateNodes.item(i).cloneNode(true);
+			svgRoot.insertBefore(templateNode, svgRoot.getFirstChild());
+		}
+		
+		super.stream(svgRoot, writer, useCss, escaped);
+	}
+	
+}
