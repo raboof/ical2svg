0b91863d5b15f84a191b2d69885de4353beb73d8
arnout.engelen <arnout.engelen@c732d7ce-d5e6-11dd-918e-25ffe7a15323>
2009-01-03 18:13:10 +0000

    some tests for future reference - for now we have just worked around the issue.

---
+++ trunk/ical2svg/src/test/java/net/bzzt/ical2svg/FontTest.java
@@ -0,0 +1,68 @@
+package net.bzzt.ical2svg;
+
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
+import java.io.StringWriter;
+
+import net.bzzt.ical2svg.SVGGraphics2DFactory;
+
+import org.apache.batik.svggen.SVGGraphics2D;
+import org.apache.batik.svggen.SVGGraphics2DIOException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import sun.awt.motif.X11FontMetrics;
+import sun.font.FontDesignMetrics;
+
+import junit.framework.TestCase;
+
+/**
+ * Under openjdk, the calculation of boundingbox heights seems to be enourmously
+ * inaccurately, I'm getting measurements similar to 
+ *  http://www.mail-archive.com/2d-dev@openjdk.java.net/msg00312.html
+ *  
+ * assertions are commented out, because for now we have worked around the issue
+ * by looking at the font point size instead.
+ * 
+ * @author arnouten
+ *
+ */
+public class FontTest extends TestCase {
+	private static final Log LOG = LogFactory.getLog(FontTest.class);
+	
+	public void testOpenJdkText() throws SVGGraphics2DIOException
+	{
+		String text = "Test";
+		
+		//Font
+		FontRenderContext frc = BlockSchemaPainter.getFontRenderContext();
+		Font font = BlockSchemaPainter.getFont("Arial", 7);
+		assertNotNull(font);
+
+		// openjdk for some reason reports '6.3125', while sun reports '5.09375'.
+		// sun seems to be (more) correct: in inkscape, 'Test' in Arial 7pt has
+		// a height of 5.202
+//		assertTrue(new TextLayout(text, font, frc).getBounds().getHeight() < 5.3);
+	}
+	
+	public void testOpenJdkTextIsolated() throws SVGGraphics2DIOException
+	{
+		String text = "The Quick Brown Fox Jumped Over The Lazy Dog.";
+		
+		FontRenderContext frc = new FontRenderContext(null, false, false);
+		Font font = new Font("Tahoma", Font.PLAIN, 11);
+		assertNotNull(font);
+
+		FontDesignMetrics.getMetrics(font).getHeight();
+		
+		// openjdk reports '18.234375', while sun reports '10.734375'.
+		// This is roughly equivalent to the measurements at
+		// http://www.mail-archive.com/2d-dev@openjdk.java.net/msg00306.html ,
+		// which might suggest (further in the thread) absence of byte code 
+		// hinting in the freetype found by OpenJDK
+		double height = new TextLayout(text, font, frc).getBounds().getHeight();
+//		assertEquals("Height is " + height, 10, height);
+	}
+}
